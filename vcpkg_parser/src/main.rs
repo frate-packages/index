0b9334ef9fb90b96e797e85b1852dfeda4fb9dac
lucas.sproule <lucas.sproule.42@gmail.com>
2023-10-26 14:06:08 -0400

    add repos with info.json

--- vcpkg_parser/src/main.rs
+++ vcpkg_parser/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use reqwest::blocking::get;
 use serde::{Deserialize, Serialize};
 use clap::Parser;
@@ -31,7 +33,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     // Parse the file name or default to sample.json
-    let mut output_file_name = String::from("sample.json");
+    let mut output_file_name = String::from("../index.json");
     if let Some(out_name) = args.out_file {
         output_file_name = out_name
     }
@@ -46,11 +48,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("[#] Sorting...");
     for item in resp.Source.iter() {
+
+        println!("[#] Checking: {:?}",item);
         if let Some(val) = item.Homepage.clone() {
             if val.contains("github") {
                 filtered.push(item);
             }
         }
+    }
+//jump back a directory and create a folder with filtered Name and save a json with Name,
+    //Homepage in the directory
+    for item in filtered.iter() {
+        let mut path = String::from("../");
+        path.push_str(&item.Name);
+        std::fs::create_dir_all(path.clone())?;
+        path.push_str("/info.json");
+        let mut file = std::fs::File::create(path)?;
+        let string = serde_json::to_string(item)?;
+        file.write_all(string.as_bytes())?;
     }
 
     // stringify json and write to file
