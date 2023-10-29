c6f07542a69dcb4029ce88b8f10a055a9aff0b8e
n3m0 <groen_b@proton.me>
2023-10-29 02:50:34 +0200

    added descriptions:w

---
+++ archived/scraper/scraper/src/main.rs
@@ -0,0 +1,82 @@
+#![allow(unused)]
+mod conan;
+mod prelude;
+mod vcpkg;
+
+use std::{collections::HashSet, io::Write};
+
+use conan::Conan;
+use prelude::Package;
+use vcpkg::Vcpkg;
+
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser, Debug)]
+#[command(author = "Brainfart", version, about, long_about = None)]
+struct Args {
+    /// Output directory
+    #[arg(short, action)]
+    output_directory: String,
+
+    /// Scrape Vcpkg
+    #[arg(short, action)]
+    vcpkg: bool,
+
+    /// Scrape Conan
+    #[arg(short, action)]
+    conan: bool,
+}
+
+fn generate_json(data: &HashSet<Package>) {
+    let mut file = std::fs::write("../something.json", serde_json::to_string(&data).unwrap());
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let mut set = HashSet::new();
+
+    let mut vcpkg = Vcpkg::new();
+    let mut conan = Conan::new();
+
+    let mut vcpkg_data = Vec::new();
+    let mut conan_data = Vec::new();
+
+    if args.vcpkg {
+        println!("[#] Starting scrapper on vcpkg");
+        vcpkg_data = vcpkg.scrape()?;
+        for data in vcpkg_data {
+            set.insert(data);
+        }
+    }
+
+    if args.conan {
+        println!("[#] Starting scrapper on conan");
+        conan_data = conan.scrape()?;
+        for data in conan_data {
+            if data.git.is_some() {
+                set.insert(data);
+            }
+        }
+    }
+
+    // iter over set if dir with name does not exist,
+    // create it and add info.json with package data
+
+    println!("[#] Creating directories");
+    generate_json(&set);
+
+    for data in set.iter() {
+        if let Ok(_) = std::fs::create_dir_all(format!("{}/{}", args.output_directory, data.name)) {
+            println!("[#] Creating: {}", data.name);
+            let mut file = std::fs::File::create(format!(
+                "{}/{}/info.json",
+                args.output_directory, data.name
+            ))?;
+            let string = serde_json::to_string(data)?;
+            file.write_all(string.as_bytes());
+        }
+    }
+    Ok(())
+}
---
+++ archived/scraper/scraper/src/prelude.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Hash)]
+#[allow(non_snake_case)]
+pub struct Package {
+    pub name: String,
+    pub git: Option<String>,
+    pub description: String,
+}
